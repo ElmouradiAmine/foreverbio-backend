8d0d3f7b3090f47fbd24abdaa0965c280cb274c9
ElmouradiAmine <elmouradi.amine98@gmail.com>
2020-02-28 22:37:43 +0100

    Added the save user controller logic.

--- foreverbio/src/main/java/com/itwins/foreverbio/controllers/RestController.java
+++ foreverbio/src/main/java/com/itwins/foreverbio/controllers/RestController.java
@@ -1,9 +1,18 @@
 package com.itwins.foreverbio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itwins.foreverbio.models.User;
+import com.itwins.foreverbio.services.UserService;
+
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/")
 	public String index() {
@@ -13,6 +22,15 @@ public class RestController {
 	@GetMapping("/home")
 	public String home() {
 		return "home";
+	}
+	
+	@GetMapping("/save-user")
+	public String saveUser(@RequestParam String userName, @RequestParam String firstName, 
+			@RequestParam String lastName, @RequestParam int age, @RequestParam String password) {
+		
+		User user = new User(userName,firstName,lastName,age,password);
+		userService.saveUser(user);
+		return "User saved successfully";
 	}
 
 	
