b2664589d6490d0f5a950d3da6945cabaca2f9ff
ElmouradiAmine <elmouradi.amine98@gmail.com>
2020-02-28 22:00:05 +0100

    Created the UserSerivice in the services Package.

--- foreverbio/src/main/java/com/itwins/foreverbio/services/UserService.java
+++ foreverbio/src/main/java/com/itwins/foreverbio/services/UserService.java
@@ -1,5 +1,22 @@
 package com.itwins.foreverbio.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class UserService {
+	
+	private UserRepository userRepository;
+	
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
 
 }
